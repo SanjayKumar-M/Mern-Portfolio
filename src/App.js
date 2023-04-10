4a09a27b3c6ba222e6607cccef4ae95480a3025c
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-04-10 14:39:13 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import './App.css'
 import { Link } from 'react-router-dom'
 const App = () => {
   return (
-    <div className='content'>
-      <h1>Click Here to View Projects</h1>
-        <ul>
-          <li><Link to='/calculator'>Calculator</Link></li>
-          <li><Link to='/api'>API Fetching</Link></li>
-        </ul>
+    <div>
+       <nav>
+          <div>
+              <p>MERN</p>
+          </div>
+          <ul>
+            
+            <li><Link to='/calculator'>Calculator</Link></li>
+            <li><Link to='/api'>API</Link></li>
+            
+            
+          </ul>
+        </nav>
+        <div className='content'>
+          <h2>Hey There! I'm Sanjay Kumar</h2> 
+          <h3>Web Developer and Smart Contract Engineer</h3>
 
+        </div>
     </div>
   )
 }
